f8941c7fc812db6cbd08a76e9239fef553c51a30
Leonardo Lima <leonardotlima2@gmail.com>
2016-10-13 08:51:57 -0300

    Adiciona AplicaLookAndFeel.java

---
+++ src/main/java/br/com/saojudas/maven/projetointegrado/view/AplicaLookAndFeel.java
@@ -0,0 +1,20 @@
+package br.com.saojudas.maven.projetointegrado.view;
+
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+public class AplicaLookAndFeel {
+	private AplicaLookAndFeel() {
+	}
+
+	public static void lookAndFeel() {
+		try {
+            // select Look and Feel
+            UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+            // start application
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+}
